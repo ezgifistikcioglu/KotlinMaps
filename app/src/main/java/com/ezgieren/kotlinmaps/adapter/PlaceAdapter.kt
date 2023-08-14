6bbd8fc7da98d4d048cdd8f06b4f1545659db249
Ezgi Fıstıkçıoğlu <fistikciogluezgi@gmail.com>
2023-08-14 14:21:30 +0300

    RecyclerView-Adapter steps are completed.

---
+++ app/src/main/java/com/ezgieren/kotlinmaps/adapter/PlaceAdapter.kt
@@ -0,0 +1,35 @@
+package com.ezgieren.kotlinmaps.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ezgieren.kotlinmaps.databinding.RecyclerRowBinding
+import com.ezgieren.kotlinmaps.model.Place
+import com.ezgieren.kotlinmaps.view.MapsActivity
+
+class PlaceAdapter(val placeList: List<Place>) : RecyclerView.Adapter<PlaceAdapter.PlaceHolder>() {
+    class PlaceHolder(val recyclerRowBinding: RecyclerRowBinding) :
+        RecyclerView.ViewHolder(recyclerRowBinding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceHolder {
+        val recyclerRowBinding =
+            RecyclerRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PlaceHolder(recyclerRowBinding)
+    }
+
+    override fun onBindViewHolder(holder: PlaceHolder, position: Int) {
+       holder.recyclerRowBinding.idTVRecyclerView.text = placeList[position].name
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, MapsActivity::class.java)
+
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return placeList.size
+    }
+}
--- app/src/main/java/com/ezgieren/kotlinmaps/view/MainActivity.kt
+++ app/src/main/java/com/ezgieren/kotlinmaps/view/MainActivity.kt
@@ -5,12 +5,42 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.ezgieren.kotlinmaps.R
+import com.ezgieren.kotlinmaps.adapter.PlaceAdapter
+import com.ezgieren.kotlinmaps.databinding.ActivityMainBinding
+import com.ezgieren.kotlinmaps.model.Place
+import com.ezgieren.kotlinmaps.roomdb.PlaceDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //RoomDatabase
+        val database = Room.databaseBuilder(applicationContext, PlaceDatabase::class.java, "Places").build()
+        val placeDao = database.placeDao()
+        //RxJava
+        compositeDisposable.add(
+            placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleResponse)
+        )
+    }
+
+    private fun handleResponse(placeList: List<Place>){
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = PlaceAdapter(placeList)
+        binding.recyclerView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
